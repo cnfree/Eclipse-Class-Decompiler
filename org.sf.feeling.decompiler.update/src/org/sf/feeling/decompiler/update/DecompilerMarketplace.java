e96a1a75f913c246589877498b3b62bba7e3c75f
cnfree <cnfree2000@hotmail.com>
2016-05-18 17:18:21 +0800

    Fixed update bug.

---
+++ org.sf.feeling.decompiler.update/src/org/sf/feeling/decompiler/update/DecompilerMarketplace.java
@@ -0,0 +1,13 @@
+
+package org.sf.feeling.decompiler.update;
+
+import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceDropAdapter;
+
+public class DecompilerMarketplace extends MarketplaceDropAdapter
+{
+
+	public void proceedInstallation( String url )
+	{
+		super.proceedInstallation( url );
+	}
+}
--- org.sf.feeling.decompiler.update/src/org/sf/feeling/decompiler/update/DecompilerUpdateHandler.java
+++ org.sf.feeling.decompiler.update/src/org/sf/feeling/decompiler/update/DecompilerUpdateHandler.java
@@ -24,8 +24,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.epp.internal.mpc.core.service.DefaultMarketplaceService;
 import org.eclipse.epp.internal.mpc.core.service.Node;
-import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceUrlHandler;
-import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceUrlHandler.SolutionInstallationInfo;
 import org.eclipse.equinox.internal.p2.metadata.OSGiVersion;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.Version;
@@ -79,65 +77,59 @@ public class DecompilerUpdateHandler implements IDecompilerUpdateHandler
 						.getPreferenceStore( )
 						.getString( DecompilerUpdatePlugin.NOT_UPDATE_VERSION ) ) )
 		{
-			final SolutionInstallationInfo info = MarketplaceUrlHandler.createSolutionInstallInfo( "http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=472922" ); //$NON-NLS-1$
-			if ( info != null )
-			{
-				Display.getDefault( ).asyncExec( new Runnable( ) {
+			Display.getDefault( ).asyncExec( new Runnable( ) {
 
-					public void run( )
+				public void run( )
+				{
+					MessageDialogWithToggle dialog = new MessageDialogWithToggle(
+							Display.getDefault( ).getActiveShell( ),
+							Messages.getString( "DecompilerUpdateHandler.ConfirmDialog.Title" ), //$NON-NLS-1$
+							null, // accept the default window icon
+							Messages.getString( "DecompilerUpdateHandler.ConfirmDialog.Message" ), //$NON-NLS-1$
+							MessageDialog.CONFIRM,
+							new String[]{
+									Messages.getString( "DecompilerUpdateHandler.ConfirmDialog.Button.NotNow" ), //$NON-NLS-1$
+									Messages.getString( "DecompilerUpdateHandler.ConfirmDialog.Button.Continue" ) //$NON-NLS-1$
+					},
+							1,
+							Messages.getString( "DecompilerUpdateHandler.ConfirmDialog.Button.NotAskAgain" ), //$NON-NLS-1$
+							false );
+					int result = dialog.open( );
+
+					if ( dialog.getToggleState( ) )
 					{
-						MessageDialogWithToggle dialog = new MessageDialogWithToggle( Display.getDefault( )
-								.getActiveShell( ),
-								Messages.getString("DecompilerUpdateHandler.ConfirmDialog.Title"), //$NON-NLS-1$
-								null, // accept the default window icon
-								Messages.getString("DecompilerUpdateHandler.ConfirmDialog.Message"), //$NON-NLS-1$
-								MessageDialog.CONFIRM,
-								new String[]{
-										Messages.getString("DecompilerUpdateHandler.ConfirmDialog.Button.NotNow"), Messages.getString("DecompilerUpdateHandler.ConfirmDialog.Button.Continue") //$NON-NLS-1$ //$NON-NLS-2$
-								},
-								1,
-								Messages.getString("DecompilerUpdateHandler.ConfirmDialog.Button.NotAskAgain"), //$NON-NLS-1$
-								false );
-						int result = dialog.open( );
-
-						if ( dialog.getToggleState( ) )
-						{
-							DecompilerUpdatePlugin.getDefault( )
-									.getPreferenceStore( )
-									.setValue( DecompilerUpdatePlugin.NOT_UPDATE_VERSION,
-											version );
-						}
-
-						if ( result - IDialogConstants.INTERNAL_ID == 1 )
-						{
-							MarketplaceUrlHandler.triggerInstall( info );
-						}
+						DecompilerUpdatePlugin.getDefault( )
+								.getPreferenceStore( )
+								.setValue( DecompilerUpdatePlugin.NOT_UPDATE_VERSION, version );
 					}
-				} );
-			}
+
+					if ( result - IDialogConstants.INTERNAL_ID == 1 )
+					{
+						new DecompilerMarketplace( ).proceedInstallation(
+								"http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=472922" ); //$NON-NLS-1$
+					}
+				}
+			} );
 		}
 	}
 
-	private String getUpdateVersion( IProgressMonitor monitor )
-			throws Exception
+	private String getUpdateVersion( IProgressMonitor monitor ) throws Exception
 	{
-		DefaultMarketplaceService service = new DefaultMarketplaceService( new URL( "http://marketplace.eclipse.org" ) ); //$NON-NLS-1$
+		DefaultMarketplaceService service = new DefaultMarketplaceService(
+				new URL( "http://marketplace.eclipse.org" ) ); //$NON-NLS-1$
 		Node node = new Node( );
 		node.setId( "472922" ); //$NON-NLS-1$
 		node = service.getNode( node, monitor );
 
-		ProvisioningSession session = ProvisioningUI.getDefaultUI( )
-				.getSession( );
+		ProvisioningSession session = ProvisioningUI.getDefaultUI( ).getSession( );
 		IMetadataRepositoryManager manager = (IMetadataRepositoryManager) session.getProvisioningAgent( )
 				.getService( IMetadataRepositoryManager.SERVICE_NAME );
 
 		URI updateUrl = new URI( node.getUpdateurl( ) );
-		IMetadataRepository repository = manager.loadRepository( updateUrl,
-				monitor );
+		IMetadataRepository repository = manager.loadRepository( updateUrl, monitor );
 		IQuery<IInstallableUnit> query = QueryUtil.createMatchQuery( "id ~= /*.feature.group/ && " + //$NON-NLS-1$
 				"properties['org.eclipse.equinox.p2.type.group'] == true " );//$NON-NLS-1$
-		IQueryResult<IInstallableUnit> result = repository.query( query,
-				monitor );
+		IQueryResult<IInstallableUnit> result = repository.query( query, monitor );
 
 		for ( Iterator<IInstallableUnit> iterator = result.iterator( ); iterator.hasNext( ); )
 		{
